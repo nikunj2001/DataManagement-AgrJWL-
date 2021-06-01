564235f76c3eb770db677fb5edc098b540d729d8
nikunj gupta <59887608+nikunj2001@users.noreply.github.com>
2021-06-01 16:08:09 +0530

    trying very haed

--- server/services/render.js
+++ server/services/render.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config('.env')
 const axios = require('axios')
 exports.homeRoutes=(req,res)=>{
-    axios.get('http://localhost:8000/api/users').then(response=>{
+    axios.get(`http://localhost:${process.env.PORT}/api/users`).then(response=>{
     res.render('index.ejs',{users:response.data});
     }).catch(err=>{
         res.send(err);
@@ -10,10 +12,10 @@ exports.add_user=(req,res)=>{
     res.render('add_user.ejs');
 }
 exports.update_user=(req,res)=>{
-    axios.get('http://localhost:8000/api/users',{params:{id:req.query.id}}).then(userData=>{
+    axios.get(`http://localhost:${process.env.PORT}/api/users`,{params:{id:req.query.id}}).then(userData=>{
             res.render('update_user.ejs',{user:userData.data});
 
     }).catch(err=>{
         res.send(err);
-    })
+    })  
 }
