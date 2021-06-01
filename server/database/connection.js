9e8f92bdeb87981777753f4f483f93546cfe4a5d
nikunj gupta <59887608+nikunj2001@users.noreply.github.com>
2021-06-01 14:22:09 +0530

    Hell Life

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose')
+const {Pool,Client} = require('pg');
+const connectionString = 'mongodb+srv://nikunj:nikunj@cluster0.n5t54.mongodb.net/users?retryWrites=true&w=majority'
+ const pool = new Pool({
+        connectionString: connectionString,
+    })
+        var client = new Client({
+        user: "nikunj",
+        password: "nikunj",
+        database: "users",
+        port: 5432,
+        host: "host",
+        ssl: true
+    });
+    client.connect(function () {
+        console.log("connected");
+    });
 const connectDB = async()=>{
     try {
-       const con= await mongoose.connect(process.env.MONGO_URL,{useUnifiedTopology:true,useNewUrlParser:true});
+       const con= await mongoose.connect(connectionString,{useUnifiedTopology:true,useNewUrlParser:true});
        console.log(`mongodb connection ${con.connection.host}`);
     } catch (error) {
         console.log(err);
